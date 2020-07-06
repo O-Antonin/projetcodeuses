08ad4b928f1135eb9a63b2f88c770f6d318c9a50
Olivia ANTONIN <oliviantonin@gmail.com>
2020-07-06 11:43:45 +0200

    modif security controller admin et users forms

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -2,13 +2,19 @@
 
 namespace App\Entity;
 
-use App\Repository\UsersRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UsersRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UsersRepository::class)
+ * @UniqueEntity (
+ * fields = {"email"},
+ *  message = "Un compte est déja existant à cette adresse mail!" )
  */
-class Users
+class Users implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -19,18 +25,31 @@ class Users
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage = "Votre mot de passe doit contenir 8 caractères minimum")
+     * @Assert\EqualTo(propertyPath="confirm_password", message="Les mots de passe ne correspondent pas")
      */
     private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Les mots de passe ne correspondent pas")
+     */
+    public $confirm_password;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
 
     public function getId(): ?int
     {
@@ -72,4 +91,40 @@ class Users
 
         return $this;
     }
+
+  /**
+     *  Pour pouvoir encoder le mot de passe, il faut que notre entité User implemente l'interface UserInterface
+     * Cette interface contient des méthodes que nous sommes obligés de déclarer:
+     * getPassword(), getUsername(), getRoles(), getSalt(), et eraseCredentials()
+     */
+
+    public function eraseCredentials()
+    {
+
+    }
+
+      // Renvoit la chaine de caractères non encodée que le user a saisi qui a été utilisé à l'origine pour encoder le mdp
+   public function getSalt()
+    {    
+       
+    } 
+    
+      // cette méthode renvoit un tableau de chaine de caractères où sont stockés les rôles accordés à l'utilisateur
+    public function getRoles()
+    {
+          //return ['ROLE_USER'];
+          return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+
+
+
+
 }
