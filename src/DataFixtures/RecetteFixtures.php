96bb01164a81ab17b2d62c55eab908c14e442f48
Olivia ANTONIN <oliviantonin@gmail.com>
2020-07-07 09:58:38 +0200

    ajout comments,navigation conditionnelle

--- src/DataFixtures/RecetteFixtures.php
+++ src/DataFixtures/RecetteFixtures.php
@@ -4,9 +4,10 @@ namespace App\DataFixtures;
 
 
 use Faker\Factory;
+use App\Entity\Comment;
 use App\Entity\Recette;
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class RecetteFixtures extends Fixture
 {
@@ -30,6 +31,30 @@ class RecetteFixtures extends Fixture
                         // aujour'hui
                         //->setCategory($category); // on relie nos articles aus catégories crées juste au desus (clé étrangère)
                 $manager->persist($recette);
+
+                // Création entre 4 et 10 commentaires par recette
+                for($k =1; $k <= mt_rand(4,10); $k++)
+
+                {   // On instancie l'entité comment afin d'insérer des commentaires dans la BDD
+                    $comment = new Comment;
+
+                    $content = '<p>'   .join($faker->paragraphs(2), '<p></p>') .  '<p>';
+                      
+                    $now = new \DateTime;
+                    $interval = $now->diff($recette->getCreatedAt()); //Représente le temps en timestamps entre la date de création de l'article et maintenant
+                    $days = $interval->days; //nombre de jour entre la date de création de la recette et maintenant
+                    $minimum = '-' .$days . ' days';
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween($minimum))
+                            ->setRecette($recette); // On relie nos commentaires aux articles crées ci-dessus ( clé étrangère)
+
+                    $manager->persist($comment); // On prépare l'insertion des commentaires
+
+
+
+                }
 
             }    
       
--- src/Migrations/Version20200706131317.php
+++ src/Migrations/Version20200706131317.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200703123949 extends AbstractMigration
+final class Version20200706131317 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,10 @@ final class Version20200703123949 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, recette_id INT NOT NULL, author VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_9474526C89312FE9 (recette_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE recette (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, roles JSON NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C89312FE9 FOREIGN KEY (recette_id) REFERENCES recette (id)');
     }
 
     public function down(Schema $schema) : void
@@ -31,6 +33,8 @@ final class Version20200703123949 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C89312FE9');
+        $this->addSql('DROP TABLE comment');
         $this->addSql('DROP TABLE recette');
         $this->addSql('DROP TABLE users');
     }
